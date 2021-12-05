bbe7af5b014647a1eb5a8b19d114478d4a596dae
ghassenbns <ghassenbenslimane00@gmail.com>
2021-12-05 21:42:00 +0100

    Fixing create post headers

--- api/post/create.php
+++ api/post/create.php
@@ -1,9 +1,17 @@
 <?php 
   // Headers
-  header('Access-Control-Allow-Origin: *');
-  header('Content-Type: application/json');
-  header('Access-Control-Allow-Methods: POST');
-  header('Access-Control-Allow-Headers: Access-Control-Allow-Headers,Content-Type,Access-Control-Allow-Methods, typeization, X-Requested-With');
+  if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+        header('Access-Control-Allow-Origin: *');
+        header('Access-Control-Allow-Methods: POST, GET, DELETE, PUT, PATCH, OPTIONS');
+        header('Access-Control-Allow-Headers: token, Content-Type');
+        header('Access-Control-Max-Age: 1728000');
+        header('Content-Length: 0');
+        header('Content-Type: application/json');
+        die();
+    }
+
+    header('Access-Control-Allow-Origin: *');
+    header('Content-Type: application/json');
 
   include_once '../../config/db.php';
   include_once '../../models/post.php';
